03d3b0e8d61e7eb59cabc3e4a0e4929b73b5937c
Nemanja Kazic <kazicnemanj1@gmail.com>
2018-05-12 14:55:27 +0200

    Dodato dugme za tvitovanje citata, ispravljeno:sada nije hard codovan prvi citat vec se i on fetchuje

--- quote.js
+++ quote.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
-
+  //Promenljive za cuvanje citata
+    var trenutniQuote;
+    var trenutniAutor;
+  //GET request za citat na pocetku
+    $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/random", function(a) {
+          $("#citat").html("<p>''" + a.quote +"''</p>" );
+          $("#autor1").html("<p>-" + a.author +"</p>" );
+          trenutniQuote=a.quote;
+          trenutniAutor=a.autor;
+    });
+  //GET request za novi citat pritiskom na dugme
     $("#dugme").on("click", function(){
        $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/random", function(a) {
           $("#citat").html("<p>''" + a.quote +"''</p>" );
           $("#autor1").html("<p>-" + a.author +"</p>" );
+          trenutniQuote=a.quote;
+          trenutniAutor=a.autor;
        });
     });
+
+  //Postavljanje citata na Twitter
+    $("#tviter").on("click", function(){
+        window.location.href = "http://twitter.com/home?status="+"''"+trenutniQuote+"''"+ "   -"+trenutniAutor; 
+    });
+
+
    
 });
 
