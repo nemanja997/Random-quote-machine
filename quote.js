eb11c95cdc3a1160e0f4cd09eab38548eb396648
Nemanja Kazic <kazicnemanj1@gmail.com>
2018-06-23 14:40:55 +0200

    changed Quotes API provider because previus one crashed

--- quote.js
+++ quote.js
@@ -19,11 +19,13 @@ $(document).ready(function() {
 
 //Funkcija za GET request citata
 function uzmiCitat(){
-  $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/random", function(a) {
+  
+    $.getJSON("https://talaikis.com/api/quotes/random/", function(a) {
+ 
           $("#citat").html("<p>''" + a.quote +"''</p>" );
           $("#autor1").html("<p>-" + a.author +"</p>" );
           trenutniQuote=a.quote;
-          trenutniAutor=a.autor;
-    });
+          trenutniAutor=a.author;
+});
 };
 
