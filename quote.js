b46bd43285c225a4c239db5ba0e90437cd80f47d
Nemanja <kazicnemanja1@gmail.com>
2019-09-26 17:58:20 +0200

    Hook new API for random quotes, minor css fixes for text sizing

--- quote.js
+++ quote.js
@@ -4,45 +4,37 @@ var trenutniAutor;
 
 $(document).ready(function() {
   
-  //Uzimanje citata na pocetku
+    //Uzimanje citata na pocetku
     uzmiCitat();
-  //Uzimanje citata pritiskom na dugme
+
+    //Uzimanje citata pritiskom na dugme
     $("#dugmeQuote").on("click", function(){
-      
-      $("#citatDiv").fadeOut(10, function(){
-       
-        $(this).fadeIn("slow");
-      uzmiCitat();
-    });
-    $("#authorDiv").fadeOut(10, function(){
-       
-      $(this).fadeIn("slow");
-    uzmiCitat();
-  });
-        
-      
-      
-      
+        $("#citatDiv").fadeOut(10, function(){
+            $(this).fadeIn("slow");
+            uzmiCitat();
+        });
+
+        $("#authorDiv").fadeOut(10, function(){
+            $(this).fadeIn("slow");
+            uzmiCitat();
+        });
     });
   //Postavljanje citata na Twitter
     $("#dugmeTweet").on("click", function(){
+      var twitterURL = "http://twitter.com/home?status="+"pozdrav"+ "   -"+trenutniAutor;
         /*window.location.href = "http://twitter.com/home?status="+"''"+trenutniQuote+"''"+ "   -"+trenutniAutor; */
-        window.open("http://twitter.com/home?status="+"''"+trenutniQuote+"''"+ "   -"+trenutniAutor, '_blank');
+        window.open(twitterURL, '_blank');
     });
   
 });
 
 //Funkcija za GET request citata
 function uzmiCitat(){
-  
-    $.getJSON("https://talaikis.com/api/quotes/random/", function(a) {
- 
-          $("#citatDiv").html( '"'+ a.quote +'"' );
-          $("#authorDiv").html("-" + a.author );
-          
-      
-          trenutniQuote=a.quote;
-          trenutniAutor=a.author;
-});
+    $.getJSON("https://api.quotable.io/random", function(a) {
+        $("#citatDiv").html( '"'+ a.content +'"' );
+        $("#authorDiv").html("-" + a.author );
+        trenutniQuote=a.quote;
+        trenutniAutor=a.author;
+    });
 };
 
