03b8578b4b71a0835490294487f092cd3c13fce3
Nemanja <kazicnemanja1@gmail.com>
2019-10-30 00:36:02 +0100

    New URL for twitter posting

--- quote.js
+++ quote.js
@@ -9,21 +9,17 @@ $(document).ready(function() {
 
     //Uzimanje citata pritiskom na dugme
     $("#dugmeQuote").on("click", function(){
-        $("#citatDiv").fadeOut(10, function(){
+        uzmiCitat();
+        $("#citatDiv, #authorDiv").fadeOut(10, function(){
             $(this).fadeIn("slow");
-            uzmiCitat();
-        });
-
-        $("#authorDiv").fadeOut(10, function(){
-            $(this).fadeIn("slow");
-            uzmiCitat();
         });
     });
+    
   //Postavljanje citata na Twitter
     $("#dugmeTweet").on("click", function(){
-      var twitterURL = "http://twitter.com/home?status="+"pozdrav"+ "   -"+trenutniAutor;
-        /*window.location.href = "http://twitter.com/home?status="+"''"+trenutniQuote+"''"+ "   -"+trenutniAutor; */
-        window.open(twitterURL, '_blank');
+        console.log(trenutniQuote);
+      var twitterURL = "https://twitter.com/intent/tweet?text="+ trenutniQuote + "   - " + trenutniAutor;
+        window.open(twitterURL);
     });
   
 });
@@ -33,7 +29,7 @@ function uzmiCitat(){
     $.getJSON("https://api.quotable.io/random", function(a) {
         $("#citatDiv").html( '"'+ a.content +'"' );
         $("#authorDiv").html("-" + a.author );
-        trenutniQuote=a.quote;
+        trenutniQuote=a.content;
         trenutniAutor=a.author;
     });
 };
